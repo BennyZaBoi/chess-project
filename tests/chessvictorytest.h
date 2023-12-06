87a07c487d163f57a889dd5779a1011b2d7dad98
BennyZaBoi <mpham115@ucr.edu>
2023-12-05 17:29:32 -0800

    Test for Chess Victory Board

---
+++ tests/chessvictorytest.h
@@ -0,0 +1,22 @@
+#include "gtest/gtest.h"
+#include "../header/chessvictory.h"
+using namespace std;
+
+TEST(ChessVictoryTests, displayVictoryScreen) {
+    // Capture standard output
+    testing::internal::CaptureStdout();
+
+    displayVictoryScreen();
+
+    // Get the captured output
+    string output = testing::internal::GetCapturedStdout();
+
+    string expectedOutput =
+        "*************************************\n"
+        "*        Congratulations!           *\n"
+        "*        You've Won the Game!       *\n"
+        "*************************************\n";
+
+    EXPECT_EQ(output, expectedOutput);
+}
+
